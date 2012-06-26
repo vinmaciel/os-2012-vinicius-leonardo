996ea401a78fdfb5b58b4f9bf9a961f5190416ae
maciel.mvinicius@gmail.com <maciel.mvinicius@gmail.com@34a944e7-ae83-8b16-b8a9-3b68598dad2a>
2012-06-26 22:04:38 +0000

    Basic hardware requirements.

---
+++ trunk/ os-2012-vinicius-leonardo --username maciel.mvinicius@gmail.com/PCS2453/src/hw/Disc.java
@@ -0,0 +1,49 @@
+package hw;
+import java.util.LinkedList;
+import os.Job;
+
+public class Disc {
+	private boolean status;
+	private LinkedList<Job> queue;
+
+	private static boolean FREE = true;
+	private static boolean BUSY = false;
+	
+	Disc() {
+		status = FREE;
+		queue = new LinkedList<Job>();
+	}
+	
+	/**
+	 * Processes an I/O request, busying it.
+	 */
+	public void assign() {
+		this.status = BUSY;
+	}
+	
+	/**
+	 * Finishes the I/O request, getting the disc free.
+	 */
+	public void release() {
+		this.status = FREE;
+	}
+	
+	/**
+	 * Insert the job in the queue if disc is busy.
+	 * 
+	 * @param job to be inserted.
+	 */
+	public void enqueue(Job job) {
+		this.queue.addLast(job);
+	}
+	
+	/**
+	 * Removes the job from the top of the queue, and executes the I/O request.
+	 */
+	public void dequeue() {
+		if(!this.queue.isEmpty()) {
+			Job job = this.queue.removeFirst();
+			// TODO: issue
+		}
+	}
+}
