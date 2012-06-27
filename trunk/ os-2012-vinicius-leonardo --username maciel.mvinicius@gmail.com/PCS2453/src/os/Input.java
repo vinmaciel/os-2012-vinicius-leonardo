66ba5c48de688a8413aab8bfba2af271fe77115e
maciel.mvinicius@gmail.com <maciel.mvinicius@gmail.com@34a944e7-ae83-8b16-b8a9-3b68598dad2a>
2012-06-27 22:43:24 +0000

    Several hardware fixes. Great software development. Added a little input example.

--- trunk/ os-2012-vinicius-leonardo --username maciel.mvinicius@gmail.com/PCS2453/src/os/Input.java
+++ trunk/ os-2012-vinicius-leonardo --username maciel.mvinicius@gmail.com/PCS2453/src/os/Input.java
@@ -21,11 +21,12 @@ public class Input {
 	 * @param fileName is the file to be read.
 	 * @param initialTime marks the current time at the beginning of the execution.
 	 * @param endTime marks the current time at the end of the execution;
+	 * @param jobList stores all the jobs the computer can run.
 	 * @param eventList stores the events described on the file.
 	 * @throws InputReadException if the definition input file doesn't follow the model.
 	 */
-	public static void read(String fileName, int initialTime, int endTime, LinkedList<Event> eventList,
-			LinkedList<Job> jobList) throws InputReadException {
+	public static void read(String fileName, int initialTime, int endTime, LinkedList<Job> jobList,
+			LinkedList<Event> eventList) throws InputReadException {
 		try {
 			// open file
 			FileInputStream inStream = new FileInputStream(fileName);
@@ -34,6 +35,9 @@ public class Input {
 			DataInputStream in = new DataInputStream (inStream);
 			BufferedReader reader = new BufferedReader (new InputStreamReader(in));
 			String line;
+			
+			// get the first comment line
+			line = reader.readLine();
 			
 			// get initial and end time
 			line = reader.readLine();
@@ -79,30 +83,15 @@ public class Input {
 					int arrivalTime = Integer.parseInt(arrival[1]);
 					
 					// search for the job
-					Job job = null;
-					ListIterator<Job> jobIterator = jobList.listIterator();
-					boolean found = false;
-					while(jobIterator.hasNext() && !found) {
-						job = jobIterator.next();
-						if(jobId == job.getId())
-							found = true;
-					}
-					if(found) {
+					Job job = findJob(jobList, jobId);
+					
+					if(job != null) {
 						// if found, create the event and stores it onto the list in arrival time order
-						boolean earlier = false;
-						Event event = new Event(job, arrivalTime);
-						ListIterator<Event> eventIterator = eventList.listIterator();
-						while(eventIterator.hasNext() && !earlier) {
-							Event eventAux = eventIterator.next();
-							if(arrivalTime < eventAux.getArrivalTime()) {
-								eventAux = eventIterator.previous();
-								earlier = true;
-							}
-						}
-						eventIterator.add(event);
+						Event event = new Event(job, arrivalTime, Event.ARRIVAL);
+						event.insert(eventList);
 					}
 					else
-						System.out.println("Invalid job with id " + jobId + ".");
+						System.out.println("Invalid job with id: " + jobId + ".");
 				}
 				
 				line = reader.readLine();
@@ -116,4 +105,26 @@ public class Input {
 			System.err.println("Error: " + e.getMessage());
 		}
 	}
+	
+	/**
+	 * Searches if the job was already defined.
+	 * 
+	 * @param jobList contains all jobs created.
+	 * @param jobId identifies the job being searched.
+	 * @return the job with jobId; or null if not found.
+	 */
+	private static Job findJob(LinkedList<Job> jobList, int jobId) {
+		Job job = null;
+		ListIterator<Job> jobIterator = jobList.listIterator();
+		
+		while(jobIterator.hasNext()) {
+			job = jobIterator.next();
+			if(jobId == job.getId())
+				return job;
+		}
+		
+		return job;
+	}
+	
+	
 }
--- trunk/ os-2012-vinicius-leonardo --username maciel.mvinicius@gmail.com/PCS2453/src/os/Scheduler.java
+++ trunk/ os-2012-vinicius-leonardo --username maciel.mvinicius@gmail.com/PCS2453/src/os/Scheduler.java
@@ -4,6 +4,8 @@ import hw.*;
 import java.util.LinkedList;
 
 public class Scheduler {
+	static final String[] ACTIONS = {};
+	
 	private int initialTime;
 	private int finalTime;
 	
@@ -14,12 +16,12 @@ public class Scheduler {
 	private LinkedList<Event> eventList;
 	private LinkedList<Job> jobList;
 	
-	private static final int MEMORY_SIZE = 512;
+	private static final int MEMORY_SIZE = 64;
 	
 	Scheduler() {
 		eventList = new LinkedList<Event>();
 		try {
-			Input.read("input.txt", initialTime, finalTime, eventList, jobList);
+			Input.read("input.txt", initialTime, finalTime, jobList, eventList);
 		}
 		catch(InputReadException e) {
 			e.printStackTrace();
@@ -27,5 +29,68 @@ public class Scheduler {
 		memory = new Memory(MEMORY_SIZE);
 		processor = new Processor();
 		disc = new Disc();
+	}
+	
+	/**
+	 * Executes all the events stored into the eventList.
+	 */
+	public void run() {
+		// starts timing
+		int currentTime = this.initialTime;
+		
+		// get the first event arriving
+		Event currentEvent = Event.getNew(eventList);
+		currentTime = currentEvent.getTime();
+		
+		// print table header
+		System.out.println("\n\nExecution:");
+		System.out.println("Time   Event                            Job  Action       Results");
+		
+		// while there are more events, and current time didn't surpass the end of times
+		while(currentEvent != null || currentTime > this.finalTime) {
+			switch(currentEvent.getType()) {
+			// job arrives
+			case Event.ARRIVAL:
+				System.out.println();
+				break;
+				
+			// job requests memory allocation
+			case Event.REQUEST_MEMORY:
+				
+				break;
+			
+			// job requests processor execution
+			case Event.REQUEST_PROCESSOR:
+				
+				break;
+			
+			// job requests I/O operation
+			case Event.REQUEST_DISC:
+				
+				break;
+			
+			// job releases disc, returning to processor
+			case Event.RELEASE_DISC:
+				
+				break;
+			
+			// job releases memory and processor
+			case Event.RELEASE_MEMORY:
+				
+				break;
+			
+			// job completes its execution
+			case Event.COMPLETE:
+				
+				break;
+			
+			// others...
+			default:
+				System.out.println("Invalid event.");
+			}
+			
+			// get the following event
+			currentEvent = Event.getNew(eventList);
+		}
 	}
 }
