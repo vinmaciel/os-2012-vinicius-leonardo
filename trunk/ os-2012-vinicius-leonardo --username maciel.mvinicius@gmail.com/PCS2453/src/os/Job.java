01fafeccba9d07cfbfb2c6d99b87a85997cc8875
maciel.mvinicius@gmail.com <maciel.mvinicius@gmail.com@34a944e7-ae83-8b16-b8a9-3b68598dad2a>
2012-06-28 00:20:03 +0000

    Scheduler (sort of) running.

--- trunk/ os-2012-vinicius-leonardo --username maciel.mvinicius@gmail.com/PCS2453/src/os/Job.java
+++ trunk/ os-2012-vinicius-leonardo --username maciel.mvinicius@gmail.com/PCS2453/src/os/Job.java
@@ -13,7 +13,39 @@ public class Job {
 		this.ioRequests = io;
 	}
 	
+	/**
+	 * Gets this job's numeric identification.
+	 * 
+	 * @return job's id.
+	 */
 	public int getId() {
 		return this.id;
+	}
+	
+	/**
+	 * Gets this job's memory space required for execution.
+	 * 
+	 * @return job's memory space required.
+	 */
+	public int getSize() {
+		return this.memorySpace;
+	}
+	
+	/**
+	 * Gets this job's total processing time.
+	 * 
+	 * @return job's processing time.
+	 */
+	public int getProcessingTime() {
+		return this.processigTime;
+	}
+	
+	/**
+	 * Gets this job's quantity of I/O requests.
+	 * 
+	 * @return job's i/o requests.
+	 */
+	public int getIoRequests() {
+		return this.ioRequests;
 	}
 }
